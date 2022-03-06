cc2f2063ee2baea723280dcd355a8e7c60d9f542
SiamakMirifar <studmirisi2219@stud.th-rosenheim.de>
2022-03-06 01:35:21 +0100

    Update main base on new changes

--- Main.py
+++ Main.py
@@ -1,21 +1,41 @@
+# Autor : Siamak Mirifar
+# Liscence : Student project ( Open source Liscence)
+
+# Version number: 1.0.1      Date: 05.01.2022
+
+# TODO: 1- Find Robot Id base on the robot color
+# TODO: 2- Find Robot location in the field
+# TODO: 3- Find Robot oriantation in the field
+# TODO: 4- Find Robot ball position (similar to robot position)
+# TODO: 5- Make DOCUMETS from where the Idea of the method has been taken !! IMPORTANT
+
+# FIXME: @start_capturing FPS does not update
+
 from ControlCommand import *
 
-
+# Definition of slots in this class are for futures if there were need to use GUI
 class CaptureImage():
     def __init__(self):
         self.img_counter = 0
 
-        self.cameraConfig = load_json_config_file()  # Loading Camera Configuration from Json File
+        self.cameraConfig       = load_json_config_file()  # Loading Camera Configuration from Json File
+        self.robot_id           = None                     # Robot Id base on Color       
+        self.robot_location     = None                     # Robot location on the field
+        self.robot_orientation  = None                     # Robot oriantation
+
 
     def __del__(self):
-        finish_capturing_command()
+        """ Destroy All the class objects """
+        finish_capturing_command(self)
 
-    def start_capturing(self):
+    def slot_start_capturing(self):
+        """ start capturing """
         start_capturing_command(self)
 
-    def finish_capturing(self):
+    def slot_finish_capturing(self):
+        """ Delete Cv2 class to empty the buffer """
         finish_capturing_command(self)
 
 
 cam = CaptureImage()
-cam.start_capturing()
+cam.slot_start_capturing()

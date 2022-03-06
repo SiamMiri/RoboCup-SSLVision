81a45e633227f147ad0a9e68c72bb5fd8dd69b45
SiamakMirifar <studmirisi2219@stud.th-rosenheim.de>
2022-03-06 01:34:23 +0100

    capturing moved to ImageProcessing class

--- ControlCommand.py
+++ ControlCommand.py
@@ -1,5 +1,6 @@
 import json
 import cv2
+from ImageProcessing import imgProc
 
 
 # load json file as dictionary in python
@@ -18,50 +19,19 @@ def load_json_config_file():
     return data
 
 
+# Function: Calling Class ImageProcessing
+# slot: start_capturing
+# Return: self.robot_id, self.robot_location, self.robot_orientation 
+# Date of issue: 05.03.2021
+# Date of modify: XX.XX.xXXx
 def start_capturing_command(self):
-    cam = cv2.VideoCapture(0)
+    """ Calles Class ImageProcessing object """
+    imgProc.start_capturing(self.cameraConfig)
 
-    # Change FPS
-    if isinstance(self.cameraConfig["FPS"], int):
-        cam.set(cv2.CAP_PROP_FPS, self.cameraConfig["FPS"])  # set camera FPS from Json file
-    elif isinstance(self.cameraConfig["FPS"], float):
-        cam.set(cv2.CAP_PROP_FPS, int(self.cameraConfig["FPS"]))  # set camera FPS from Json file
-    else:
-        print(".json file config FPS is not correct")
+    self.robot_id          = imgProc.detect_robot_id()
+    self.robot_location    = imgProc.detect_robot_location()
+    self.robot_orientation = imgProc.detect_robot_orientation()
 
-    # Change Resolution
-    if isinstance(self.cameraConfig["Resolution"], list):
-        # set camera Resolution from Json file
-        cam.set(3, int(self.cameraConfig["Resolution"][0]))
-        cam.set(4, int(self.cameraConfig["Resolution"][1]))
-    else:
-        print(".json file config Resolution is not correct")
+def finish_capturing_command(self):
+    imgProc.finish_capturing()
 
-    # Change Focus
-    if isinstance(self.cameraConfig["focus"], int):
-        # set camera Resolution from Json file
-        cam.set(cv2.CAP_PROP_FOCUS, self.cameraConfig["focus"])
-    elif isinstance(self.cameraConfig["focus"], float):
-        # set camera Resolution from Json file
-        cam.set(cv2.CAP_PROP_FOCUS, int(self.cameraConfig["focus"]))
-    else:
-        cam.set(cv2.CAP_PROP_AUTOFOCUS, 0)
-
-    cv2.namedWindow("RobotSoccer\tHit Escape to Exit")
-
-    while True:
-        ret, frame = cam.read()
-        if not ret:
-            print("failed to grab frame")
-            break
-        cv2.imshow("RobotSoccer\tHit Escape to Exit", frame)
-
-        k = cv2.waitKey(1)
-        if k % 256 == 27:
-            # ESC pressed
-            print("Escape hit, closing...")
-            break
-
-
-def finish_capturing_command():
-    cv2.destroyAllWindows()

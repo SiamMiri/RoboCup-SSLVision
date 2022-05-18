faa49e69151a19d86d028bc52786725a93ea8d69
studmirisi2219 <studmirisi2219@stud.th-rosenheim.de>
2022-05-18 20:35:12 +0200

    Color Picker

--- HSV_Color_Picker.py
+++ HSV_Color_Picker.py
@@ -1,106 +1,98 @@
 from __future__ import print_function
 import cv2 as cv
 import argparse
-max_value = 255
-max_value_H = 360//2
-low_H = 0
-low_S = 0
-low_V = 0
-high_H = max_value_H
-high_S = max_value
-high_V = max_value
-window_capture_name = 'Video Capture'
-window_detection_name = 'Object Detection'
-<<<<<<< HEAD
-low_H_name = 'Low H'
-low_S_name = 'Low S'
-low_V_name = 'Low V'
-=======
-low_H_name  = 'Low H'
-low_S_name  = 'Low S'
-low_V_name  = 'Low V'
->>>>>>> bd61e1a7f4ee447e26f0532ed44c58a99db19f81
-high_H_name = 'High H'
-high_S_name = 'High S'
-high_V_name = 'High V'
-def on_low_H_thresh_trackbar(val):
-    global low_H
-    global high_H
-    low_H = val
-    low_H = min(high_H-1, low_H)
-    cv.setTrackbarPos(low_H_name, window_detection_name, low_H)
-def on_high_H_thresh_trackbar(val):
-    global low_H
-    global high_H
-    high_H = val
-    high_H = max(high_H, low_H+1)
-    cv.setTrackbarPos(high_H_name, window_detection_name, high_H)
-def on_low_S_thresh_trackbar(val):
-    global low_S
-    global high_S
-    low_S = val
-    low_S = min(high_S-1, low_S)
-    cv.setTrackbarPos(low_S_name, window_detection_name, low_S)
-def on_high_S_thresh_trackbar(val):
-    global low_S
-    global high_S
-    high_S = val
-    high_S = max(high_S, low_S+1)
-    cv.setTrackbarPos(high_S_name, window_detection_name, high_S)
-def on_low_V_thresh_trackbar(val):
-    global low_V
-    global high_V
-    low_V = val
-    low_V = min(high_V-1, low_V)
-    cv.setTrackbarPos(low_V_name, window_detection_name, low_V)
-def on_high_V_thresh_trackbar(val):
-    global low_V
-    global high_V
-    high_V = val
-    high_V = max(high_V, low_V+1)
-    cv.setTrackbarPos(high_V_name, window_detection_name, high_V)
 
-parser = argparse.ArgumentParser(description='Code for Thresholding Operations using inRange tutorial.')
-parser.add_argument('--camera', help='Camera divide number.', default=0, type=int)
+class HSV_COLOR_PICKER():
+    max_value = 255
+    max_value_H = 360//2
+    low_H = 0
+    low_S = 0
+    low_V = 0
+    high_H = max_value_H
+    high_S = max_value
+    high_V = max_value
+    window_capture_name = 'Video Capture'
+    window_detection_name = 'Object Detection'
+    low_H_name  = 'Low H'
+    low_S_name  = 'Low S'
+    low_V_name  = 'Low V'
+    high_H_name = 'High H'
+    high_S_name = 'High S'
+    high_V_name = 'High V'
+    
+    def __init__(self) -> None:
+        parser = argparse.ArgumentParser(description='Code for Thresholding Operations using inRange tutorial.')
+        parser.add_argument('--camera', help='Camera divide number.', default=0, type=int)
 
-args = parser.parse_args()
+        args = parser.parse_args()
 
-cap = cv.VideoCapture(args.camera)
+        self.capture = cv.VideoCapture(args.camera)
 
-cv.namedWindow(window_capture_name)
-cv.namedWindow(window_detection_name)
+        cv.namedWindow(HSV_COLOR_PICKER.window_capture_name)
+        cv.namedWindow(HSV_COLOR_PICKER.window_detection_name)
 
-cv.createTrackbar(low_H_name  , window_detection_name , low_H  , max_value_H , on_low_H_thresh_trackbar )
-cv.createTrackbar(high_H_name , window_detection_name , high_H , max_value_H , on_high_H_thresh_trackbar)
-cv.createTrackbar(low_S_name  , window_detection_name , low_S  , max_value   , on_low_S_thresh_trackbar )
+        cv.createTrackbar(HSV_COLOR_PICKER.low_H_name  , HSV_COLOR_PICKER.window_detection_name , HSV_COLOR_PICKER.low_H  , HSV_COLOR_PICKER.max_value_H , HSV_COLOR_PICKER.on_low_H_thresh_trackbar )
+        cv.createTrackbar(HSV_COLOR_PICKER.high_H_name , HSV_COLOR_PICKER.window_detection_name , HSV_COLOR_PICKER.high_H , HSV_COLOR_PICKER.max_value_H , HSV_COLOR_PICKER.on_high_H_thresh_trackbar)
+        cv.createTrackbar(HSV_COLOR_PICKER.low_S_name  , HSV_COLOR_PICKER.window_detection_name , HSV_COLOR_PICKER.low_S  , HSV_COLOR_PICKER.max_value   , HSV_COLOR_PICKER.on_low_S_thresh_trackbar )
 
-cv.createTrackbar(high_S_name , window_detection_name , high_S , max_value   , on_high_S_thresh_trackbar)
-cv.createTrackbar(low_V_name  , window_detection_name , low_V  , max_value   , on_low_V_thresh_trackbar )
-cv.createTrackbar(high_V_name , window_detection_name , high_V , max_value   , on_high_V_thresh_trackbar)
+        cv.createTrackbar(HSV_COLOR_PICKER.high_S_name , HSV_COLOR_PICKER.window_detection_name , HSV_COLOR_PICKER.high_S , HSV_COLOR_PICKER.max_value   , HSV_COLOR_PICKER.on_high_S_thresh_trackbar)
+        cv.createTrackbar(HSV_COLOR_PICKER.low_V_name  , HSV_COLOR_PICKER.window_detection_name , HSV_COLOR_PICKER.low_V  , HSV_COLOR_PICKER.max_value   , HSV_COLOR_PICKER.on_low_V_thresh_trackbar )
+        cv.createTrackbar(HSV_COLOR_PICKER.high_V_name , HSV_COLOR_PICKER.window_detection_name , HSV_COLOR_PICKER.high_V , HSV_COLOR_PICKER.max_value   , HSV_COLOR_PICKER.on_high_V_thresh_trackbar)
+    
+    def on_low_H_thresh_trackbar(self, val):
+        global low_H
+        global high_H
+        low_H = val
+        low_H = min(high_H-1, low_H)
+        cv.setTrackbarPos(HSV_COLOR_PICKER.low_H_name, HSV_COLOR_PICKER.window_detection_name, low_H)
+    def on_high_H_thresh_trackbar(self, val):
+        global low_H
+        global high_H
+        high_H = val
+        high_H = max(high_H, low_H+1)
+        cv.setTrackbarPos(HSV_COLOR_PICKER.high_H_name, HSV_COLOR_PICKER.window_detection_name, high_H)
+    def on_low_S_thresh_trackbar(self, val):
+        global low_S
+        global high_S
+        low_S = val
+        low_S = min(high_S-1, low_S)
+        cv.setTrackbarPos(HSV_COLOR_PICKER.low_S_name, HSV_COLOR_PICKER.window_detection_name, low_S)
+    def on_high_S_thresh_trackbar(self, val):
+        global low_S
+        global high_S
+        high_S = val
+        high_S = max(high_S, low_S+1)
+        cv.setTrackbarPos(HSV_COLOR_PICKER.high_S_name, HSV_COLOR_PICKER.window_detection_name, high_S)
+    def on_low_V_thresh_trackbar(self, val):
+        global low_V
+        global high_V
+        low_V = val
+        low_V = min(high_V-1, low_V)
+        cv.setTrackbarPos(HSV_COLOR_PICKER.low_V_name, HSV_COLOR_PICKER.window_detection_name, low_V)
+    def on_high_V_thresh_trackbar(self, val):
+        global low_V
+        global high_V
+        high_V = val
+        high_V = max(high_V, low_V+1)
+        cv.setTrackbarPos(HSV_COLOR_PICKER.high_V_name, HSV_COLOR_PICKER.window_detection_name, high_V)
 
-def color_picker(frame):
-    while True:
-        
-        # ret, frame = cap.read()
+    def color_picker(self, frame):
+        while True:
+            
+            ret, frame = self.capture.read()
 
-<<<<<<< HEAD
-        frame = cv.imread("t.png")
-        #frame = cv.resize(frame, (740,480))
-=======
-        frame = cv.imread('ee.jpg')
-        frame = cv.resize(frame, (740,480))
->>>>>>> bd61e1a7f4ee447e26f0532ed44c58a99db19f81
+            frame = cv.imread('Robot5_x841_y356.jpg')
+            frame = cv.resize(frame, (740,480))
 
-        #if frame is None:
-            #break
-        frame_HSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-        frame_threshold = cv.inRange(frame_HSV, (low_H, low_S, low_V), (high_H, high_S, high_V))
-        
-        
-        cv.imshow(window_capture_name, frame)
-        cv.imshow(window_detection_name, frame_threshold)
-        
-        key = cv.waitKey(30)
-        if key == ord('q') or key == 27:
-            break
-color_picker(1)
+            #if frame is None:
+                #break
+            frame_HSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+            frame_threshold = cv.inRange(frame_HSV, (HSV_COLOR_PICKER.low_H, HSV_COLOR_PICKER.low_S, HSV_COLOR_PICKER.low_V), (HSV_COLOR_PICKER.high_H, HSV_COLOR_PICKER.high_S, HSV_COLOR_PICKER.high_V))
+            
+            
+            cv.imshow(HSV_COLOR_PICKER.window_capture_name, frame)
+            cv.imshow(HSV_COLOR_PICKER.window_detection_name, frame_threshold)
+            
+            key = cv.waitKey(30)
+            if key == ord('q') or key == 27:
+                break
